da7680a0495958c71720e70161c0e0b31f4663f8
Shmuel Cohen <shmuel@yuvalp-mbp13.meetinplace>
2021-04-11 14:36:13 +0300

    Update

---
+++ Week2/Day1/Ex_XP.js
@@ -0,0 +1,63 @@
+var x = 5;
+var y = "34";
+var result = x+y;
+
+console.log(result);
+
+var a = 5;
+var b = 4;
+var result = a-b;
+console.log(result);
+
+var a = 10;
+var b = 5;
+var result = a%b;
+console.log(result);
+
+
+var a = 10;
+var b = 5;
+var result = b%a;
+console.log(result);
+
+
+var a = "Java";
+var b = "Script";
+var result = a+b;
+console.log(result);
+
+var a = " ";
+var b = " ";
+var result = a+b;
+console.log(result);
+
+var a = true;
+var b = true;
+var result = a+b;
+console.log(result);
+
+var a = true;
+var b = false;
+var result = a+b;
+console.log(result);
+
+var a = false;
+var b = true;
+var result = a+b;
+console.log(result);
+
+
+var a = false;
+var b = true;
+var result = a-b;
+console.log(result);
+
+var a = 3;
+var b = 4;
+var result = a-b;
+console.log(result);
+
+var a = "bob";
+var b = "bill";
+var result = a-b;
+console.log(result);
