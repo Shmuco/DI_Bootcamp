c703eb3ab350bc1bb4118a16f08af3bb46ad2f44
Shmuel Cohen <shmuel@Shmuels-MacBook-Pro.local>
2021-04-12 09:50:47 +0300

    update

--- Week2/Day1/DC_JS_Arrays_Methods.js
+++ Week2/Day1/DC_JS_Arrays_Methods.js
@@ -1,36 +1,38 @@
-// Exercise 1:
+// // Exercise 1:
 
-// Remove Banana from the array.
-var fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
-fruits.shift();
-console.log(fruits);
+// // Remove Banana from the array.
+// var fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+// fruits.shift();
+// console.log(fruits);
 
-// Sort the array in alphabetical order
-fruits.sort();
-console.log(fruits)
+// // Sort the array in alphabetical order
+// fruits.sort();
+// console.log(fruits)
 
-// Add “Kiwi” to the end of the array.
-fruits.push("Kiwi");
-console.log(fruits);
+// // Add “Kiwi” to the end of the array.
+// fruits.push("Kiwi");
+// console.log(fruits);
 
-// Remove “Apples” from the array. Don’t use the same method as in part 1.
-fruits.splice(0,1);
-console.log(fruits);
+// // Remove “Apples” from the array. Don’t use the same method as in part 1.
+// fruits.splice(0,1);
+// console.log(fruits);
 
-// Sort the array in reverse order. 
-// (Not alphabetical, but reverse the current Array i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])
-fruits.reverse();
-console.log(fruits);
+// // Sort the array in reverse order. 
+// // (Not alphabetical, but reverse the current Array i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])
+// fruits.reverse();
+// console.log(fruits);
 
 
 
 
 // Exercise 2:
 let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
-let fruits = moreFruits [1];
-console.log(fruits);
-let lessfruits = fruits [1];
-console.log(lessfruits);
-let oranges = lessfruits [0];
-console.log(oranges)
+// let fruits = moreFruits [1];
+// console.log(fruits);
+// let lessfruits = fruits [1];
+// console.log(lessfruits);
+// let oranges = lessfruits [0];
+// console.log(oranges)
 
+
+console.log(moreFruits [1][1][0]);
