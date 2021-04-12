15239aac6b99ec3728ecff19639e50d855c90410
Shmuel Cohen <shmuel@Shmuels-MacBook-Pro.local>
2021-04-12 15:21:51 +0300

    Update

---
+++ Week2/Day2/CN_Conditionals_JS/scripts.js
@@ -0,0 +1,43 @@
+// Exercise 1
+
+// let age = prompt("How old are you?")
+// ageInt = parseInt(age)
+// if (ageInt<18) {alert("Sorry you are too young to drive this car. Powering off")    
+// }
+// else if (ageInt===18) {alert("Congratulations on your first year of driving! Enjoy the ride!")
+// }
+// else {alert("Powering on. Enjoy the ride!")
+// }
+
+
+
+// Introduction on Objects
+
+// Exercise 1
+
+let account ={
+    Username: "Person",
+    Password: "Password"
+}
+
+console.log(account)
+
+let database =[account]
+console.log(database)
+
+
+let newsfeed = [
+    {
+        username: "User",
+        timeline: "5 years",
+    },
+    {
+        username: "User2",
+        timeline: "5 years",
+    },
+    {
+        username: "User3",
+        timeline: "5 years",
+    },]
+   
+   
