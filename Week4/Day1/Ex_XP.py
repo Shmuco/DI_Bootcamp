a6b7b275271f4aa80fab54f99f5f91a70ce72bda
Shmuel Cohen <shmuel@Shmuels-MacBook-Pro.local>
2021-04-25 14:39:30 +0300

    Update

---
+++ Week4/Day1/Ex_XP.py
@@ -0,0 +1,50 @@
+# # Ex 1
+# sen = "Hello World\n"
+# print(sen*5)
+
+
+# #Ex 2
+# print ((99**3)*8)
+
+# #Ex 4
+# computer_brand = "Mac"
+# print(f'I have a {computer_brand} computer')
+
+# #Ex 5
+# name="Shmuel"
+# age="27"
+# shoe_size="7"
+# info=f'My name is {name} i am {age} and my shoe size is {shoe_size}....just in case you were wondering'
+
+# print(info)
+
+# #Ex 6
+# a = input("Enter a number: ")
+# b = input("Enter another number: ")
+# if a>b:
+#     print ("Hello World")
+# else:
+#      (print("try again") )
+
+
+# #Ex 7
+# testnum = input("Enter a number to see if its odd or even: ")
+# if int(testnum)%2 == 0:
+#     print (f"The numer {testnum} is Even")
+# else:
+#     print(f"The numer {testnum} is odd")
+
+# #Ex 8
+# usrname = input("Whats your name?")
+# if usrname.lower() == "shmuel":
+#     print("Snap!!")
+# else:
+#     print("Meh")
+
+# Ex 9
+
+height = input("Please enter you height in Inches: ")
+if (int(height)*2.54) > 145:
+    print("You can ride")
+else:
+    (print("Come back when you've grown some more."))
