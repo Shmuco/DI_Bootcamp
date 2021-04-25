38f98603f898093d5e95747f9bedc3878a06a339
Shmuel Cohen <shmuel@Shmuels-MacBook-Pro.local>
2021-04-25 16:11:41 +0300

    update

---
+++ Week4/Day1/Ex_XP_Gold.py
@@ -0,0 +1,14 @@
+# Ex 1
+print("Hello World\n"*5+"I love python\n"*5)
+
+# Ex 2
+month=input("Enter a Month: ")
+imonth=int(month)
+if 3 <=imonth <=5:
+    print("Spring")
+elif 6 <=imonth <=8:
+    print("Summer")
+elif 9 <=imonth <=11:
+    print("Autumn")
+elif 12 ==imonth or 1<= imonth<=2:
+    print("Winter")
