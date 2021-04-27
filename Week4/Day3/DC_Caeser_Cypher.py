7c252a62efdca3df9861abfc4de7d22b75e6a0fd
Shmuel Cohen <shmuel@Shmuels-MacBook-Pro.local>
2021-04-27 21:00:47 +0300

    update

---
+++ Week4/Day3/DC_Caeser_Cypher.py
@@ -0,0 +1,26 @@
+text = input("Enter the message: ")
+option = int(input("Press 1 to Encrypt or 2 to Decrypt: "))
+cypher_text=""
+if option==1:
+    for letter in text:
+        if letter.isdigit()==True or letter.isalpha()==False:
+            cypher_text=cypher_text+letter 
+        elif ord(letter)>=121:
+            cypher_text =cypher_text + chr(ord(letter)-23)
+        else:
+            cypher_text =cypher_text + chr(ord(letter)+ 3)
+    cypher_text=cypher_text.replace("#", " ")
+
+elif option==2:
+    for letter in text:
+        if letter.isdigit()==True or letter.isalpha()==False:
+            cypher_text=cypher_text+letter 
+        elif ord(letter)<=67:
+            cypher_text =cypher_text + chr(ord(letter)+23)
+        else:
+            cypher_text =cypher_text + chr(ord(letter)- 3)
+    cypher_text=cypher_text.replace("7", " ")
+else:
+    option = input("Press 1 to Encrypt or 2 to Decrypt")
+    
+print(cypher_text)
