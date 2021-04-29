4579c6b6a1b2f0cdc9684d2ea64c1305d77d6a45
Shmuel Cohen <shmuel@Shmuels-MacBook-Pro.local>
2021-04-29 21:26:43 +0300

    update

--- Week3/Day1/DC_Planets.js
+++ Week3/Day1/DC_Planets.js
@@ -17,4 +17,60 @@ for (i in planets) {
     
 }
 
+if (planets[i]=="earth"){
+    let x =0 
+while (x<1){
+let moon = document.createElement("div")
+moon.classList.add("moon") 
+document.body.appendChild(moon)
+x++
+
+}
+
+}
+
+if (planets[i]=="jupiter"){
+    let x =0 
+while (x<79){
+let moon = document.createElement("div")
+moon.classList.add("moon") 
+document.body.appendChild(moon)
+x++
+
+}
+
+}
+if (planets[i]=="neptune"){
+    let x =0 
+while (x<14){
+let moon = document.createElement("div")
+moon.classList.add("moon") 
+document.body.appendChild(moon)
+x++
+
+}
+
+}
+if (planets[i]=="saturn"){
+    let x =0 
+while (x<62){
+let moon = document.createElement("div")
+moon.classList.add("moon") 
+document.body.appendChild(moon)
+x++
+
+}
+
+}
+if (planets[i]=="uranus"){
+    let x =0 
+while (x<27){
+let moon = document.createElement("div")
+moon.classList.add("moon") 
+document.body.appendChild(moon)
+x++
+
+}
+
+}
 }
