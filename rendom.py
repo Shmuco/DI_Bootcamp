98520f62cc834ae38e732da6545d322788af3d67
Shmuel Cohen <shmuel@Shmuels-MacBook-Pro.local>
2021-05-09 21:33:26 +0300

    Update

--- rendom.py
+++ rendom.py
@@ -6,9 +6,15 @@
 
 # print(f'{username}, your password {password_block}, is {plength} charecters long')
 
-school = {'Bobby','Tammy','Jammy','Sally','Danny'}
-attendance_list = ['Jammy', 'Bobby', 'Danny', 'Sally']
+# school = {'Bobby','Tammy','Jammy','Sally','Danny'}
+# attendance_list = ['Jammy', 'Bobby', 'Danny', 'Sally']
 
-for name in school:
-    if name not in attendance_list:
-        print(name)
+# for name in school:
+#     if name not in attendance_list:
+#         print(name)
+
+
+some_list = ['a','b','c','b','d','m','n','n']
+
+duplicates = list(set([i for i in some_list if some_list.count(i)>1 ]))
+print(duplicates)
