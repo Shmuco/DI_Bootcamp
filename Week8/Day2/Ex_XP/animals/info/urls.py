83661007ce658a7a17b78906ba418c05b87f4074
Shmuel Cohen <shmuel@Shmuels-MacBook-Pro.local>
2021-05-27 22:40:47 +0300

    update

--- Week8/Day2/Ex_XP/animals/info/urls.py
+++ Week8/Day2/Ex_XP/animals/info/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('animals/', views.animals, name='animals'),
-    path('animal/<int:animal_id>', views.single_animal, name='single_animals'),
-    
-    # path('animal/<int:animal_id>', views.animal_id, name='animal_id'),
-    # path('family/<int:family_id>', views.family_id, name = 'family_id'),
-]
+    path('animals/', views.all_animals, name='animals'),
+    path('animal/<int:animal_id>/', views.single_animal, name='single_animal'),
+    path('family/<int:family_id>/', views.single_family, name = 'single_family'),
+    ]
--- Week8/Day2/Ex_XP/animals/info/views.py
+++ Week8/Day2/Ex_XP/animals/info/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from .models import Animal,Family
 
-def animals(request):
-    animals = Animal.objects.all()
-    return render(request, 'all_animals.html', {'all_animals': animals})
+# not printing on HTML
+def all_animals(request):
+    all_animals = Animal.objects.all()
+    for i in all_animals:
+        print(i.name)
+    return render(request, 'all_animals.html', {'all_animals':all_animals})
 
 def single_animal(request,animal_id):
     animal = Animal.objects.get(id=animal_id)
     print('hello')
     return render(request, 'single_animal.html', {'animal': animal})
 
-# Create your views here.
+def single_family(request,family_id):
+    print('hello')
+    family = Animal.objects.filter(family=family_id)
+    return render(request, 'single_family.html', {'family':family})
